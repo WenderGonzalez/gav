ef75fbabf9b135326d831c9bf58bf151f2f4401e
WenderGonzalez <wendergonzalez099@gmail.com>
2025-05-23 23:05:09 -0500

    Update SecurityConfig.java

--- src/main/java/com/proyecto/gav/config/SecurityConfig.java
+++ src/main/java/com/proyecto/gav/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.proyecto.gav.config;
 
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
